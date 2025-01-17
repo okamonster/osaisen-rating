36b6d61acdcfabaa29f93686812fb692a05e7e55
okamonster <okamonster0615@gmail.com>
2025-01-17 23:02:45 +0900

    fix:del import

--- apps/backend/src/routes/usdJpyRate.ts
+++ apps/backend/src/routes/usdJpyRate.ts
@@ -1,7 +1,5 @@
-import { eq } from 'drizzle-orm'
 import { drizzle } from 'drizzle-orm/d1'
 import { Hono } from 'hono'
-import { UsdJpyYearsRates } from '~/drizzle/schema'
 import { getAllUsdJpyRateOperation } from '~/features/rates/operations/getAllUsdJpyRate'
 import { getUsdJpyRateByYearOperation } from '~/features/rates/operations/getUsdJpyRateByYear'
 
