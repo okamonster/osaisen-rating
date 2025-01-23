fff3412af3be542903473c5c0501408a4e776fe9
okamonster <okamonster0615@gmail.com>
2025-01-24 08:52:28 +0900

    feat:add help page

--- apps/osaisen/src/app/help/page.tsx
+++ apps/osaisen/src/app/help/page.tsx
@@ -6,26 +6,32 @@ import styles from './page.module.css'
 export default function Page() {
 	return (
 		<div className={styles.container}>
-			<Image
-				src="/images/main-logo.png"
-				alt="ご利益レーティング"
-				width={500}
-				height={500}
-				className={styles.mainLogo}
-			/>
+			<h1 className={styles.mainLogo}>あけましておめでとうございます</h1>
+
+			<p className={styles.description}>
+				昨年は良い年になったでしょうか
+				<br />
+				初詣は行かれましたか？
+				<br />
+				初詣といえば...そう、お賽銭ですね
+				<br />
+				<br />
+				昨年といえば、円安の1年でした
+				<br />
+				きっと神様はグローバルな価値観を持っているはずです
+				<br />
+				円安が進んでいるのに本当に去年と同じお賽銭金額でいいのでしょうか？
+				<br />
+				<br />
+				このアプリでは過去のお賽銭金額を入力すると、その当時のお賽銭のご利益をキープするためには、今年はいくらのお賽銭が必要かを計算できる優れもの！
+				<br />
+				ぜひ試してみてください！
+			</p>
+
 			<div className={styles.actions}>
 				<Link href="/rates">
 					<Button color="var(--button-primary-color)" w="100%">
 						はじめる
-					</Button>
-				</Link>
-				<Link href="/help">
-					<Button
-						color="var(--button-primary-color)"
-						w="100%"
-						variant="outline"
-					>
-						使い方
 					</Button>
 				</Link>
 			</div>
