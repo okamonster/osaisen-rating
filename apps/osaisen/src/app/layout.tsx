72c715dc8839b4c8332378608c7faf3e1ede1881
okamonster <okamonster0615@gmail.com>
2025-01-24 09:25:52 +0900

    feat:add seo

--- apps/osaisen/src/app/layout.tsx
+++ apps/osaisen/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import { Notifications } from '@mantine/notifications'
 import '@mantine/notifications/styles.css'
+import Head from 'next/head'
 import styles from './layout.module.css'
 
 export default function RootLayout({
@@ -14,6 +15,24 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="ja">
+			<Head>
+				<meta charSet="utf-8" lang="ja" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>お賽銭レーティング</title>
+				<meta property="og:title" content="お賽銭レーティング" />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://osaisen-rating.com" />
+				<meta
+					property="og:image"
+					content="https://osaisen-rating.com/images/ogp.png"
+				/>
+				<meta
+					property="og:description"
+					content="お賽銭レーティングは、過去のお賽銭金額を入力すると、その当時のお賽銭のご利益をキープするためには、今年はいくらのお賽銭が必要かを計算できるアプリです。"
+				/>
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@makura_nageru" />
+			</Head>
 			<body className={styles.container}>
 				<MantineProvider>
 					<Notifications position="top-center" />
