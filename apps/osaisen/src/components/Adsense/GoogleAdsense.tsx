b0424bb5d5a8da13f8ec2f9b4a42323857444ebb
okamonster <okamonster0615@gmail.com>
2025-02-14 05:22:56 +0900

    feat:add adsense

---
+++ apps/osaisen/src/components/Adsense/GoogleAdsense.tsx
@@ -0,0 +1,22 @@
+import Script from 'next/script'
+import type { ReactNode } from 'react'
+
+type Props = {
+	pId: string
+}
+
+const GoogleAdsense = ({ pId }: Props): ReactNode => {
+	if (process.env.NODE_ENV !== 'production') {
+		return null
+	}
+	return (
+		<Script
+			async
+			src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+			crossOrigin="anonymous"
+			strategy="afterInteractive"
+		/>
+	)
+}
+
+export default GoogleAdsense
